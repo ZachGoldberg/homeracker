1cea6479b8d080f80c9736f273b72f18333f92ca
Zach Goldberg <zach@zachgoldberg.com>
2026-03-04 12:49:45 -0800

    Add libraries to path

--- cmd/linux/openscad-wrapper.sh
+++ cmd/linux/openscad-wrapper.sh
@@ -5,5 +5,5 @@ WORKSPACE_ROOT="$(cd "${SCRIPT_DIR}/../.." && pwd)"
 OPENSCAD_DIR="${WORKSPACE_ROOT}/bin/openscad"
 
 export QT_QPA_PLATFORM=xcb
-export OPENSCADPATH="${OPENSCAD_DIR}/libraries"
+export OPENSCADPATH="${OPENSCAD_DIR}/libraries:${WORKSPACE_ROOT}/models"
 exec "${OPENSCAD_DIR}/OpenSCAD.AppImage" "$@"
